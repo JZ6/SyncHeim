00979c01a5f38e5905a83cd65bbeb1619ec30ac5
JZ <yjzuo6@gmail.com>
2021-04-16 15:55:11 -0400

    updated

--- sync.py
+++ sync.py
@@ -4,13 +4,15 @@
 from pathlib import Path
 
 def to_local(world_name):
+	valheim_dir = Path(user_home,'AppData','LocalLow','IronGate','Valheim')
+	pass
+	 
+
+def to_remote():
 	user_home = Path.home()
 	valheim_dir = Path(user_home,'AppData','LocalLow','IronGate','Valheim')
 
 	glob_pattern = f'worlds/{world_name}.*'
 	for world in valheim_dir.glob(glob_pattern):
 		print(world)
-	 
 
-def to_remote():
-	pass
