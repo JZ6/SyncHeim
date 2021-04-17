6fc417d78d88bb0c0705fe76a4bb0fc078e44973
lucidsushi <snowbow2@gmail.com>
2021-04-16 21:54:45 -0400

    - patch: su local path test

--- sync.py
+++ sync.py
@@ -3,16 +3,25 @@
 
 from pathlib import Path
 
+user_home = Path.home()
+valheim_dir = Path(user_home,'AppData','LocalLow','IronGate','Valheim')
+if not valheim_dir.exists():
+	valheim_dir = Path('/mnt/c/Users/thesu/', 'AppData', 'LocalLow', 'IronGate', 'Valheim')
+
 def to_local(world_name):
-	valheim_dir = Path(user_home,'AppData','LocalLow','IronGate','Valheim')
 	pass
-	 
 
-def to_remote():
-	user_home = Path.home()
-	valheim_dir = Path(user_home,'AppData','LocalLow','IronGate','Valheim')
-
+def to_remote(world_name: str):
 	glob_pattern = f'worlds/{world_name}.*'
 	for world in valheim_dir.glob(glob_pattern):
+		if 'steam_autocloud' in world.as_posix():
+			continue
 		print(world)
 
+'''
+sushinocode@All-You-Can-Eat ~/repo/SyncHeim (su_local_no_freedom_to_reach.wip)$ python -c 'import sync; sync.to_remote("gme2moon")'
+/mnt/c/Users/thesu/AppData/LocalLow/IronGate/Valheim/worlds/gme2moon.db
+/mnt/c/Users/thesu/AppData/LocalLow/IronGate/Valheim/worlds/gme2moon.db.old
+/mnt/c/Users/thesu/AppData/LocalLow/IronGate/Valheim/worlds/gme2moon.fwl
+/mnt/c/Users/thesu/AppData/LocalLow/IronGate/Valheim/worlds/gme2moon.fwl.old
+'''
